1b17e54deb34a24d38ccfa61f56ea4a533794e2c
SolaticSolutions <10873670+spikeyduy@users.noreply.github.com>
2023-01-21 16:23:57 -0700

    Right click menu is populated with selected text and does an Amazon search

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
-function redPage() {
-    document.body.style.backgroundColor = 'red';
-}
-
-chrome.action.onClicked.addListener((tab) => {
-    if(!tab.url.includes("reddit.com")) {
-        chrome.scripting.executeScript({
-            target: {tabId: tab.id},
-            function: redPage
-        });
-    }
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        id: "AmazonSearch",
+        title: "Search Amazon for '%s'",
+        type: "normal",
+        contexts: ["selection"]
+    });
 });
 
+chrome.contextMenus.onClicked.addListener(function(item, tab) {
+    let url = "https://www.amazon.com/s?k=" + item.selectionText;
+    chrome.tabs.create({url: url, index: tab.index + 1});
+});
